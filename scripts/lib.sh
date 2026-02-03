70e488fa6cd643315b5448f89cac8f2c7d26a2cd
Dmitry Porotov <dmitryporotov@gmail.com>
2026-02-03 16:16:19 +0200

    saving lib.sh content

---
+++ scripts/lib.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+######################################
+# created by: Dmitry Porotov
+# purpose: common functions
+# version: 0.0.1
+# date: 2026-02-03
+###################################### 
+
+
+function output_and_log() {
+    echo $2 | tee -a $LOG_FILE_PATH >&$1
+}
+
+function check_for_root() {
+    if [[ $EUID != 0 ]]; then
+        echo >&2 "This script needs root privelegies to run"
+        exit 1
+    fi
+}
